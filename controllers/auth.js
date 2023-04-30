36c89ef3b854cf88a44d76d83a771e9ac7ededf5
Daniel Durán <danieldurange@outlook.com>
2023-04-30 17:24:09 -0500

    Google SignIn añadido

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { response } = require("express");
 const Usuario = require("../models/usuario");
 const bcryptjs = require("bcryptjs");
 const { generarJWT } = require("../helpers/generarJWT");
+const { googleVerify } = require("../helpers/googleVerify");
 
 const login = async (req, res = response) => {
   const { correo, password } = req.body;
@@ -36,6 +37,50 @@ const login = async (req, res = response) => {
   }
 };
 
+const googleSignIn = async (req, res) => {
+  const { id_token } = req.body;
+
+  try {
+    const { nombre, img, correo } = await googleVerify(id_token);
+    // const googleUser = await googleVerify(id_token);
+    let usuario = await Usuario.findOne({ correo });
+
+    if (!usuario) {
+      const data = {
+        nombre,
+        correo,
+        password: ":P",
+        img,
+        google: true,
+        rol: "USER_ROLE",
+      };
+
+      usuario = new Usuario(data);
+      await usuario.save();
+    }
+
+    if (!usuario.estado) {
+      return res.status(401).json({
+        msg: "Hable con el administrador, usuario bloqueado",
+      });
+    }
+
+    const token = await generarJWT(usuario.id);
+
+    res.json({
+      usuario,
+      token,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).json({
+      ok: false,
+      msg: "El Token no se pudo verificar",
+    });
+  }
+};
+
 module.exports = {
   login,
+  googleSignIn,
 };
